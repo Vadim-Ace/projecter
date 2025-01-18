5a9a2e64ae0b6291f75d2182f30424351cb49af2
Vadim Eismont <vacemont@gmail.com>
2025-01-18 23:42:34 +0400

    just 2 commit

--- the_first.py
+++ the_first.py
@@ -1,3 +1,4 @@
 print('Hello GitHub!')
-print('I've just created my first file in my first project here at GitHub! ')
+print('Ive just created my first file in my first project here at GitHub! ')
+print("I like VSCode")
 
